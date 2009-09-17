3784ab93a1083292022c0b99e6740ff24d3c6736
fmucar <fmucar@bfe3c2e6-bb45-11dd-a503-ed2d4bea8bb5>
2009-09-17 00:49:10 +0000

    Menu01 Completed

--- trunk/WicketMenu/src/com/cooldatasoft/common/MenuItem.java
+++ trunk/WicketMenu/src/com/cooldatasoft/common/MenuItem.java
@@ -1,34 +1,45 @@
 package com.cooldatasoft.common;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.wicket.markup.html.WebPage;
 
 public class MenuItem {
 
-	private String linkText;
+	private String text;
 	private WebPage destinationPage;
 	private List<MenuItem> subMenuItemList;
+	private boolean seperator = false;
+	private boolean title = false;
 	
 
+	public MenuItem(boolean seperator){
+		setSeperator(true);
+	}
+	public MenuItem(String title){
+		setTitle(true);
+		setText(title);
+	}
 	public <T extends WebPage>MenuItem(String linkText, T destinationPage) {
-		setLinkText(linkText);
+		setText(linkText);
 		setDestinationPage(destinationPage);
+		setSubMenuItemList(new ArrayList<MenuItem>());
 	}
 	public MenuItem(String linkText, Class<? extends WebPage> destinationWebPage,List<MenuItem> subMenuItemList) throws InstantiationException, IllegalAccessException {
 		this(linkText,destinationWebPage.newInstance(),subMenuItemList);
 	}
 	public <T extends WebPage>MenuItem(String linkText, T destinationPage,List<MenuItem> subMenuItemList) {
-		setLinkText(linkText);
+		setText(linkText);
 		setDestinationPage(destinationPage);
 		setSubMenuItemList(subMenuItemList);
 	}
 	
-	public String getLinkText() {
-		return linkText;
+	public String getText() {
+		return text;
 	}
-	public void setLinkText(String linkText) {
-		this.linkText = linkText;
+	public void setText(String text) {
+		this.text = text;
 	}
 	public WebPage getDestinationPage() {
 		return destinationPage;
@@ -41,5 +52,17 @@ public class MenuItem {
 	}
 	public void setSubMenuItemList(List<MenuItem> subMenuItemList) {
 		this.subMenuItemList = subMenuItemList;
+	}
+	public boolean isSeperator() {
+		return seperator;
+	}
+	public void setSeperator(boolean seperator) {
+		this.seperator = seperator;
+	}
+	public boolean isTitle() {
+		return title;
+	}
+	public void setTitle(boolean title) {
+		this.title = title;
 	}
 }
--- trunk/WicketMenu/src/com/cooldatasoft/menu01/Menu01.java
+++ trunk/WicketMenu/src/com/cooldatasoft/menu01/Menu01.java
@@ -1,12 +1,18 @@
 package com.cooldatasoft.menu01;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ResourceReference;
 import org.apache.wicket.markup.html.IHeaderContributor;
 import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.image.ContextImage;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.html.resources.CompressedResourceReference;
 import org.apache.wicket.model.Model;
@@ -19,6 +25,8 @@ public class Menu01 extends Panel implements IHeaderContributor {
 	ContextImage bgLeft = new ContextImage("bgLeft",new Model( "resources/com.cooldatasoft.menu01.Menu01/images/nav-bg-l.jpg"));
 	ContextImage bgRight = new ContextImage("bgRight",new Model( "resources/com.cooldatasoft.menu01.Menu01/images/nav-bg-r.jpg"));
 	
+	
+	
 	public Menu01(String id, List<MenuItem> menuItemList) {
 		super(id);
 		
@@ -28,31 +36,88 @@ public class Menu01 extends Panel implements IHeaderContributor {
 		add(bgLeft);
 		add(bgRight);
 		
-//		ListView menuView = new ListView("menuLinkList", menuListOfLinkList) {
-//			int itemCount = 0;
-//
-//			public void populateItem(final ListItem item) {
-//				
-//				final LinkInfo linkInfo = ((List<LinkInfo>) item.getModelObject()).get(0);
-//				Link link = new Link("menuLink") {
-//					@Override
-//					public void onClick() {
-//						if (linkInfo.getResponsePage() != null) {
-//							setResponsePage(linkInfo.getResponsePage());
-//						}
-//					}
-//				};
-//
-//				// Adding submenu to menu item
-//				link.add(new AttributeModifier("rel", true, new Model("dropmenu" + itemCount)));
-//				setNumberOfMenu(itemCount++);
-//
-//				Label linkText = new Label("linkText", linkInfo.getLinkText());
-//				linkText.setRenderBodyOnly(true);
-//				link.add(linkText);
-//				item.add(link);
-//			}
-//		};
+		
+		ListView primaryMenuListView = new ListView("menuItem",menuItemList){
+			@Override
+			protected void populateItem(ListItem item) {
+				final MenuItem menuItem = (MenuItem) item.getModelObject();
+				
+				Link link = new Link("menuLink") {
+					@Override
+					public void onClick() {
+						if (menuItem != null && menuItem.getDestinationPage() != null) {
+							setResponsePage(menuItem.getDestinationPage());
+						}
+					}
+				};
+				Label seperator = new Label("menuSeperator");
+				seperator.add(new AttributeModifier("class",true,new Model("divider divider-vert")));
+				
+				Label linkText = new Label("linkText");
+				link.add(new AttributeModifier("class",true,new Model("item-primary")));
+				
+				if(menuItem!=null && menuItem.getText()!=null && !menuItem.isSeperator()){
+					linkText.setModel(new Model(menuItem.getText()));
+					linkText.setRenderBodyOnly(true);
+				}				
+				link.add(linkText);
+				if(menuItem.isSeperator()){
+					link.setVisible(false);
+				}else{
+					seperator.setVisible(false);
+				}
+				
+				WebMarkupContainer subMenuList = new WebMarkupContainer("subMenuList");
+				if(menuItem.getSubMenuItemList() == null){
+					menuItem.setSubMenuItemList(new ArrayList<MenuItem>());
+				}
+				
+				ListView subMenuListView = new ListView("subMenuItem",menuItem.getSubMenuItemList()){
+					@Override
+					protected void populateItem(ListItem item) {
+						final MenuItem subMenuItem = (MenuItem) item.getModelObject();
+						
+						Link subMenuLink = new Link("subMenuLink") {
+							@Override
+							public void onClick() {
+								if (subMenuItem != null && subMenuItem.getDestinationPage() != null) {
+									setResponsePage(subMenuItem.getDestinationPage());
+								}
+							}
+						};
+						Label subMenuSeperatorOrSecondaryTitle = new Label("subMenuSeperatorOrSecondaryTitle");
+						if(subMenuItem.isSeperator()){						
+							subMenuSeperatorOrSecondaryTitle.add(new AttributeModifier("class",true,new Model("divider divider-horiz")));
+						}else if(subMenuItem.isTitle()){
+							subMenuSeperatorOrSecondaryTitle.add(new AttributeModifier("class",true,new Model("item-secondary-title")));
+							subMenuSeperatorOrSecondaryTitle.setModel(new Model(subMenuItem.getText()));
+						}						
+						Label subMenuLinkText = new Label("subMenuLinkText");						
+						if(subMenuItem!=null && subMenuItem.getText()!=null && !subMenuItem.isSeperator()){
+							subMenuLinkText.setModel(new Model(subMenuItem.getText()));
+							subMenuLinkText.setRenderBodyOnly(true);
+						}				
+						subMenuLink.add(subMenuLinkText);
+						if(subMenuItem.isSeperator() || subMenuItem.isTitle()){
+							subMenuLink.setVisible(false);
+						}else{
+							subMenuSeperatorOrSecondaryTitle.setVisible(false);
+						}						
+						item.add(subMenuLink);
+						item.add(subMenuSeperatorOrSecondaryTitle);
+						item.add(subMenuSeperatorOrSecondaryTitle);
+					}
+				};				
+				subMenuList.add(subMenuListView);
+				if(menuItem.getSubMenuItemList().size()==0){
+					subMenuList.setVisible(false);
+				}				
+				item.add(link);
+				item.add(seperator);
+				item.add(subMenuList);
+			}
+		};
+		add(primaryMenuListView);
 	}
 
 	@Override
