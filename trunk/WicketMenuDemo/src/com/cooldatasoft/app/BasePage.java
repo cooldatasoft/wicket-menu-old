2b58698fbab56d6ee440e098f055a32d291e01f6
fmucar <fmucar@bfe3c2e6-bb45-11dd-a503-ed2d4bea8bb5>
2009-09-17 23:54:14 +0000

    Added Chrome Menu

--- trunk/WicketMenuDemo/src/com/cooldatasoft/app/BasePage.java
+++ trunk/WicketMenuDemo/src/com/cooldatasoft/app/BasePage.java
@@ -1,7 +1,14 @@
 package com.cooldatasoft.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.wicket.markup.html.WebPage;
+
+import com.cooldatasoft.common.MenuItem;
+import com.cooldatasoft.page.Index;
 
 public class BasePage extends WebPage{
 
+	
 }
--- trunk/WicketMenuDemo/src/com/cooldatasoft/page/Index.java
+++ trunk/WicketMenuDemo/src/com/cooldatasoft/page/Index.java
@@ -7,13 +7,21 @@ import com.cooldatasoft.app.BasePage;
 public class Index extends BasePage {
 
 	public Index() {
-		Link menu01Demo = new Link("menu01Demo"){
+		Link sunriseGlossDropDownMenuLink = new Link("sunriseGlossDropDownMenuLink"){
 			@Override
 			public void onClick() {
 				setResponsePage(SunriseGlossDropDownMenuDemo.class);
 			}
 		};
 		
-		add(menu01Demo);
+		Link chromeDropDownMenuLink = new Link("chromeDropDownMenuLink"){
+			@Override
+			public void onClick() {
+				setResponsePage(ChromeDropDownMenuDemo.class);
+			}
+		};
+		
+		add(sunriseGlossDropDownMenuLink);
+		add(chromeDropDownMenuLink);
 	}
 }
