84589765f2da1dcb875c67c4a6a18da9767efb79
fmucar <fmucar@bfe3c2e6-bb45-11dd-a503-ed2d4bea8bb5>
2009-10-08 19:16:46 +0000

    delete

--- trunk/WicketMenu/src/com/cooldatasoft/common/DestinationType.java
+++
@@ -1,9 +0,0 @@
-package com.cooldatasoft.common;
-
-public interface DestinationType {
-
-	public static int WEB_PAGE_CLASS = 1;
-	public static int WEB_PAGE_INSTANCE = 2;
-	public static int EXTERNAL_LINK = 3;
-	public static int NONE = 4;
-}
