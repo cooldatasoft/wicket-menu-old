e8e67d57031f131e6cb0dfc07a7abf52f8a97fc3
fmucar <fmucar@bfe3c2e6-bb45-11dd-a503-ed2d4bea8bb5>
2009-11-27 17:31:14 +0000

    Demo link added

--- trunk/WicketMenu/src/com/cooldatasoft/horizontal/dropdown/multiLevelCss/MultiLevelCssMenu.java
+++ trunk/WicketMenu/src/com/cooldatasoft/horizontal/dropdown/multiLevelCss/MultiLevelCssMenu.java
@@ -9,7 +9,11 @@ import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.html.resources.CompressedResourceReference;
 
 import com.cooldatasoft.common.MenuItem;
-
+/**
+ * http://www.dynamicdrive.com/style/csslibrary/item/jquery_multi_level_css_menu_2/
+ * @author fucar
+ *
+ */
 public class MultiLevelCssMenu extends Panel implements IHeaderContributor {
 
 	private ResourceReference SHORTCUTS_JAVASCRIPT = new CompressedResourceReference(MultiLevelCssMenu.class,"js/MultiLevelCssMenu.js");
