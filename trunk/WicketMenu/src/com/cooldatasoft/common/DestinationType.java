a470d34479b03c5a243f268d29b62a0c6c5bb6da
fmucar <fmucar@bfe3c2e6-bb45-11dd-a503-ed2d4bea8bb5>
2009-11-27 16:13:17 +0000

    DestinationType contents should be final

--- trunk/WicketMenu/src/com/cooldatasoft/common/DestinationType.java
+++ trunk/WicketMenu/src/com/cooldatasoft/common/DestinationType.java
@@ -2,8 +2,8 @@ package com.cooldatasoft.common;
 
 public interface DestinationType {
 
-	public static int WEB_PAGE_CLASS = 1;
-	public static int WEB_PAGE_INSTANCE = 2;
-	public static int EXTERNAL_LINK = 3;
-	public static int NONE = 4;
+	public static final int WEB_PAGE_CLASS = 1;
+	public static final int WEB_PAGE_INSTANCE = 2;
+	public static final int EXTERNAL_LINK = 3;
+	public static final int NONE = 4;
 }
