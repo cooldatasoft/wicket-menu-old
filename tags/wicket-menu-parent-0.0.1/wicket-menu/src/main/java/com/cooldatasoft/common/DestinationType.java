2483fb406213e951512bb7d35d7142ea9d7bc5aa
fmucar@gmail.com <fmucar@gmail.com@bfe3c2e6-bb45-11dd-a503-ed2d4bea8bb5>
2012-07-26 12:24:01 +0000

    [maven-release-plugin]  copy for tag wicket-menu-parent-0.0.1

---
+++ tags/wicket-menu-parent-0.0.1/wicket-menu/src/main/java/com/cooldatasoft/common/DestinationType.java
@@ -0,0 +1,13 @@
+package com.cooldatasoft.common;
+/**
+ * 
+ * @author Fatih Mehmet UCAR - fmucar@gmail.com
+ *
+ */
+public interface DestinationType {
+
+	public static final int WEB_PAGE_CLASS = 1;
+	public static final int WEB_PAGE_INSTANCE = 2;
+	public static final int EXTERNAL_LINK = 3;
+	public static final int NONE = 4;
+}
