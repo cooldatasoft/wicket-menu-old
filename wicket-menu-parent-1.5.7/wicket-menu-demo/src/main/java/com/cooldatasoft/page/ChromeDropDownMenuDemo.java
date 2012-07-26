84c6241e0de33ccc02fd62c7d5702f7ef597af6d
fmucar <fmucar@gmail.com>
2012-07-26 13:14:38 +0000

    [maven-release-plugin]  copy for tag wicket-menu-parent-1.5.7

---
+++ wicket-menu-parent-1.5.7/wicket-menu-demo/src/main/java/com/cooldatasoft/page/ChromeDropDownMenuDemo.java
@@ -0,0 +1,54 @@
+package com.cooldatasoft.page;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cooldatasoft.app.BasePage;
+import com.cooldatasoft.common.MenuItem;
+import com.cooldatasoft.horizontal.dropdown.chrome.ChromeDropDownMenu;
+
+public class ChromeDropDownMenuDemo extends BasePage {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6195466051934462178L;
+
+	public ChromeDropDownMenuDemo() {
+		List<MenuItem> primaryMenuList = buildMenu();
+		// add your menu to your wicket page
+		add(new ChromeDropDownMenu("chromeMenu", primaryMenuList));
+	}
+
+	private List<MenuItem> buildMenu() {
+
+		// Define Primary Menu items (menuText,destinationWebPage)
+		MenuItem primaryMenu1 = new MenuItem("MENU 1", new Index());
+		MenuItem primaryMenu2 = new MenuItem("MeNu 2", new Index());
+		MenuItem primaryMenu3 = new MenuItem("Menu 3", new Index());
+		MenuItem primaryMenu4 = new MenuItem("MenU 4", new Index());
+		MenuItem primaryMenu5 = new MenuItem("menu 5", new Index());
+		// Define submenu items
+		MenuItem subMenu1 = new MenuItem("submenu 1", new Index());
+		MenuItem subMenu2 = new MenuItem("subMENU 2", new Index());
+		MenuItem subMenu3 = new MenuItem("SUBMENU 3", new Index());
+		MenuItem subMenu4 = new MenuItem("Submenu 4", new Index());
+		MenuItem subMenu5 = new MenuItem("SuBmEnU 5", new Index());
+		// Add submenus/submenu titles/seperators to the primary menu at your choice of order
+		primaryMenu1.getSubMenuItemList().add(subMenu1);
+		primaryMenu1.getSubMenuItemList().add(subMenu2);
+		primaryMenu1.getSubMenuItemList().add(subMenu3);
+		primaryMenu1.getSubMenuItemList().add(subMenu4);
+		primaryMenu1.getSubMenuItemList().add(subMenu5);
+
+		// Create a List which contains the primary menu items in it.
+		List<MenuItem> primaryMenuList = new ArrayList<MenuItem>();
+		primaryMenuList.add(primaryMenu1);
+		primaryMenuList.add(primaryMenu2);
+		primaryMenuList.add(primaryMenu3);
+		primaryMenuList.add(primaryMenu4);
+		primaryMenuList.add(primaryMenu5);
+
+		return primaryMenuList;
+	}
+}
